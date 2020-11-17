2503496e634ac51369bbfedaf490affe86239f1d
tinogis <afita@gisce.net>
2020-11-17 17:34:10 +0100

    Bump to v0.1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md') as f:
 
 setup(
     name="powerprofile",
-    version="0.0.4",
+    version="0.1.0",
     author="GISCE-TI, S.L.",
     author_email="devel@gisce.net",
     description=("Library to manage power profiles"),
