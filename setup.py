16fc211cd6ffa94af4cd65e36a855ff9b15b7641
Eduard Berloso Clarà <eberloso@gisce.net>
2021-06-28 18:25:28 +0200

    Bump to v0.8.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md') as f:
 
 setup(
     name="powerprofile",
-    version="0.7.0",
+    version="0.8.0",
     author="GISCE-TI, S.L.",
     author_email="devel@gisce.net",
     description=("Library to manage power profiles"),
