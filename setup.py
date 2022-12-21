f91a9c928ae69c43587944618bb806c42c3ff38a
davidmunoznovoa <dmunoz@gisce.net>
2022-12-21 15:52:43 +0100

    Bump to v0.14.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md') as f:
 
 setup(
     name="powerprofile",
-    version="0.14.2",
+    version="0.14.3",
     author="GISCE-TI, S.L.",
     author_email="devel@gisce.net",
     description=("Library to manage power profiles"),
