5eecba1c0db72d7a3924fc3981a3d43abb800ef6
Release <github-actions@github.com>
2024-07-08 09:51:33 +0200

    Bump to v0.23.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md') as f:
 
 setup(
     name="powerprofile",
-    version="0.22.3",
+    version="0.23.0",
     author="GISCE-TI, S.L.",
     author_email="devel@gisce.net",
     description=("Library to manage power profiles"),
