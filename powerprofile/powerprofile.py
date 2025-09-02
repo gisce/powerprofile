66ce30c138e592affbdd22b088b250f9c75508af
cesteva <cesteva@gisce.net>
2025-09-02 17:36:05 +0200

    get all holes func

--- powerprofile/powerprofile.py
+++ powerprofile/powerprofile.py
@@ -162,6 +162,29 @@ class PowerProfile():
             return False, first_not_found
         return True, None
 
+    def get_all_holes(self):
+        ''' Checks completeness of curve and returns all the holes '''
+        counter = self.hours
+        start = self.start
+        if self.start.tzinfo is None or self.start.tzinfo.utcoffset(self.start) is None:
+            start = TIMEZONE.localize(self.start)
+        end = self.end
+        if self.end.tzinfo is None or self.end.tzinfo.utcoffset(self.end) is None:
+            end = TIMEZONE.localize(self.end)
+
+        samples = ((end - start).total_seconds() + self.SAMPLING_INTERVAL) / self.SAMPLING_INTERVAL
+        if counter != samples or counter != self.unique_samples:
+            ids = set(self.curve[self.datetime_field])
+            dt = start
+            df_hours = set([TIMEZONE.normalize(dt + timedelta(seconds=x * self.SAMPLING_INTERVAL)) for x in range(0, int(samples))])
+            not_found = sorted(list(df_hours - ids))
+            if len(not_found):
+                first_not_found = not_found
+            else:
+                first_not_found = [dt]
+            return False, first_not_found
+        return True, None
+
     def is_complete(self):
         return self.is_complete_counter(self.hours)
 
